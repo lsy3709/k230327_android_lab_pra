5eece1d1a8682bee574a481d9d8760cfab3492f9
hanspung <hanspung@gmail.com>
2023-08-04 12:25:48 +0900

    테스트 커밋

---
+++ app/src/main/java/com/example/k230327_android_lab_pra/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.k230327_android_lab_pra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// 깃허브 테스트 확인용입니다
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
